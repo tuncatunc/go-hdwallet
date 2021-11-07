ba33d78a799d0fb9306e546914cda5984e5a22d2
maxifjaved <axif.javed@gmail.com>
2021-11-07 19:56:54 +0000

    tuncatunc

--- example/createRawTransaction/main.go
+++ example/createRawTransaction/main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"github.com/foxnut/go-hdwallet"
+	"github.com/tuncatunc/go-hdwallet"
 
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
